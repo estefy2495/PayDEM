32fb5455dbd338444bc89a19f2fa39adf3fd0d94
Estefania <Estefania@10.130.34.25>
2016-12-14 12:00:21 +0100

    Clases iniciales

---
+++ src/paydem/Reporte.java
@@ -0,0 +1,15 @@
+package paydem;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estefania
+ */
+public class Reporte {
+    
+}
